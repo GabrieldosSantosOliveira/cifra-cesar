53c2e91f294f56134d900a436bd9c03248535763
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-11 11:28:48 -0400

    feat: Add dark theme

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+window.addEventListener("load", setThemeOnLoad);
+
 function calc() {
   const alfabeto = [
     "A",
@@ -47,22 +49,61 @@ function calc() {
     }
   }
   console.log(codificado);
-  document.getElementById("result").textContent = "Criptografado: " + codificado;
+  document.getElementById("result").textContent =
+    "Criptografado: " + codificado;
 }
-function descript(){
+function descript() {
   let text = document.getElementById("text").value;
 
- 
-  let imprimir_descript = document.getElementById("imprimir_descript").innerHTML = "Descriptografado: " + text
-
+  let imprimir_descript = (document.getElementById(
+    "imprimir_descript"
+  ).innerHTML = "Descriptografado: " + text);
 }
-let menu = document.getElementById('menu');
-let links = document.getElementById('links');
+let menu = document.getElementById("menu");
+let links = document.getElementById("links");
 
-function Clicar(){
-  if(links.style.display == "none"){
+function Clicar() {
+  if (links.style.display == "none") {
     links.style.display = "block";
     links.style.display = "flex";
-  }else if (links.style.display = "block")
-  links.style.display = "none";
+  } else if ((links.style.display = "block")) links.style.display = "none";
+}
+function handleTheme() {
+  let body = document.querySelector("body");
+  if (body.classList.contains("theme-light")) {
+    localStorage.setItem("theme", "dark");
+    body.classList.remove("theme-light");
+    body.classList.add("theme-dark");
+  } else {
+    localStorage.setItem("theme", "light");
+    body.classList.remove("theme-dark");
+    body.classList.add("theme-light");
+  }
+}
+function setThemeOnLoad() {
+  let body = document.querySelector("body");
+
+  if (localStorage.getItem("theme") === "dark") {
+    body.classList.remove("theme-light");
+    body.classList.add("theme-dark");
+  } else if (localStorage.getItem("theme") === "light") {
+    body.classList.remove("theme-dark");
+    body.classList.add("theme-light");
+  } else {
+    setThemeDevice();
+  }
+}
+function setThemeDevice() {
+  const theme = window.matchMedia("(prefers-color-scheme: dark)");
+  let body = document.querySelector("body");
+
+  if (theme.matches) {
+    body.classList.remove("theme-light");
+    body.classList.add("theme-dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    body.classList.remove("theme-dark");
+    body.classList.add("theme-light");
+    localStorage.setItem("theme", "light");
+  }
 }
