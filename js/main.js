76d43b5c3a39f1dc34a11a4c48deff11eb148cbb
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-11 11:33:44 -0400

    fix: Placed theme logic in another file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-window.addEventListener("load", setThemeOnLoad);
-
 function calc() {
   const alfabeto = [
     "A",
@@ -67,43 +65,4 @@ function Clicar() {
     links.style.display = "block";
     links.style.display = "flex";
   } else if ((links.style.display = "block")) links.style.display = "none";
-}
-function handleTheme() {
-  let body = document.querySelector("body");
-  if (body.classList.contains("theme-light")) {
-    localStorage.setItem("theme", "dark");
-    body.classList.remove("theme-light");
-    body.classList.add("theme-dark");
-  } else {
-    localStorage.setItem("theme", "light");
-    body.classList.remove("theme-dark");
-    body.classList.add("theme-light");
-  }
-}
-function setThemeOnLoad() {
-  let body = document.querySelector("body");
-
-  if (localStorage.getItem("theme") === "dark") {
-    body.classList.remove("theme-light");
-    body.classList.add("theme-dark");
-  } else if (localStorage.getItem("theme") === "light") {
-    body.classList.remove("theme-dark");
-    body.classList.add("theme-light");
-  } else {
-    setThemeDevice();
-  }
-}
-function setThemeDevice() {
-  const theme = window.matchMedia("(prefers-color-scheme: dark)");
-  let body = document.querySelector("body");
-
-  if (theme.matches) {
-    body.classList.remove("theme-light");
-    body.classList.add("theme-dark");
-    localStorage.setItem("theme", "dark");
-  } else {
-    body.classList.remove("theme-dark");
-    body.classList.add("theme-light");
-    localStorage.setItem("theme", "light");
-  }
 }
