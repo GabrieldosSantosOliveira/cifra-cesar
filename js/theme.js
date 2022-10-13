806b0c49d72428202a09477c1d353075ad930c25
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-10-12 22:23:27 -0400

    feat: <in progress>add function to change the color of css variables

--- js/theme.js
+++ js/theme.js
@@ -6,10 +6,12 @@ function handleTheme() {
     localStorage.setItem("theme", "dark");
     body.classList.remove("theme-light");
     body.classList.add("theme-dark");
+    handleColor("dark");
   } else {
     localStorage.setItem("theme", "light");
     body.classList.remove("theme-dark");
     body.classList.add("theme-light");
+    handleColor("light");
   }
 }
 function setThemeOnLoad() {
@@ -18,9 +20,11 @@ function setThemeOnLoad() {
   if (localStorage.getItem("theme") === "dark") {
     body.classList.remove("theme-light");
     body.classList.add("theme-dark");
+    handleColor("dark");
   } else if (localStorage.getItem("theme") === "light") {
     body.classList.remove("theme-dark");
     body.classList.add("theme-light");
+    handleColor("light");
   } else {
     setThemeDevice();
   }
@@ -33,9 +37,32 @@ function setThemeDevice() {
     body.classList.remove("theme-light");
     body.classList.add("theme-dark");
     localStorage.setItem("theme", "dark");
+    handleColor("dark");
   } else {
     body.classList.remove("theme-dark");
     body.classList.add("theme-light");
     localStorage.setItem("theme", "light");
+    handleColor("light");
+  }
+}
+function handleColor(theme) {
+  if (theme === "light") {
+    document.body.style.setProperty("--text-primary", "black");
+    document.body.style.setProperty("--bg-main", "hsl(212, 45%, 77%)");
+    document.body.style.setProperty("--text-input", "hsl(218, 44%, 12%)");
+    document.body.style.setProperty(
+      "--placeholder-input",
+      "hsl(218, 44%, 12%)"
+    );
+    document.body.style.setProperty("--bg-input", "hsl(218, 44%, 99%)");
+  } else {
+    document.body.style.setProperty("--text-primary", "white");
+    document.body.style.setProperty("--bg-main", "hsl(218, 44%, 12%)");
+    document.body.style.setProperty("--text-input", "hsl(218, 44%, 12%)");
+    document.body.style.setProperty(
+      "--placeholder-input",
+      "hsl(218, 44%, 12%)"
+    );
+    document.body.style.setProperty("--bg-input", "hsl(218, 44%, 12%)");
   }
 }
