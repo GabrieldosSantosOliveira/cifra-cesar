c904c2acb883b963b07cf8a01aa64dace0cd1d38
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-26 20:46:33 -0400

    refactor: Updated to improve readability

--- js/cryptography.js
+++ js/cryptography.js
@@ -1,119 +1,97 @@
-function calc() {
-  const alfabeto = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  const texto = document.getElementById("text").value.toUpperCase().split("");
-  const spacing = document.getElementById("spacing").value;
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+function encrypt() {
+  const textForEncrypt = document
+    .getElementById("text")
+    .value.toUpperCase()
+    .split("");
 
-  const pulo = Number(spacing);
-  let codificado = "";
-  for (let i = 0; i < texto.length; i++) {
-    if (texto[i] === " ") {
-      codificado += " ";
-    } else {
-      const posicao = alfabeto.indexOf(texto[i]);
-      const novaPosicao = posicao + pulo;
+  const spacing = Number(document.getElementById("spacing").value);
+  let encrypted = "";
 
-      if (novaPosicao >= alfabeto.length) {
-        codificado += alfabeto[novaPosicao % alfabeto.length];
-      } else {
-        codificado += alfabeto[novaPosicao];
-      }
+  textForEncrypt.map((text, index) => {
+    switch (alphabet.indexOf(text)) {
+      case -1:
+        encrypted += text;
+        break;
+      default:
+        const position = alphabet.indexOf(text);
+        const newPosition = position + spacing;
+        if (newPosition >= alphabet.length) {
+          encrypted += alphabet[newPosition % alphabet.length];
+        } else {
+          encrypted += alphabet[newPosition];
+        }
+        break;
     }
-  }
-  console.log(codificado);
-  document.getElementById("result").textContent =
-    "Criptografado: " + codificado;
+  });
+
+  document.getElementById("result").textContent = "Criptografado: " + encrypted;
 }
-function descriptorCesar(texto, pulo) {
-  const alfabeto = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
+function descriptorCesar(texto, spacing) {
+  let encrypted = "";
+  texto.map((text) => {
+    switch (alphabet.indexOf(text)) {
+      case -1:
+        encrypted += text;
+        break;
+      default:
+        const position = alphabet.indexOf(text);
+        const newPosition = position - spacing;
 
-  let codificado = "";
-  for (let i = 0; i < texto.length; i++) {
-    if (texto[i] === " ") {
-      codificado += " ";
-    } else {
-      const posicao = alfabeto.indexOf(texto[i]);
-      const novaPosicao = posicao - pulo;
-
-      if (novaPosicao < 0) {
-        codificado += alfabeto[novaPosicao + alfabeto.length];
-      } else {
-        codificado += alfabeto[novaPosicao];
-      }
+        if (newPosition < 0) {
+          encrypted += alphabet[newPosition + alphabet.length];
+        } else {
+          encrypted += alphabet[newPosition];
+        }
+        break;
     }
-  }
-  return `<div>${codificado}</div>`;
+  });
+
+  return `<div>${encrypted}</div>`;
 }
 function descriptor() {
-  const text = document.getElementById("text").value;
+  const textForDecrypt = document.getElementById("decript").value.toUpperCase();
   let result = [];
 
   for (let i = 0; i < 26; i++) {
-    result.push(descriptorCesar(text, i + 1));
+    result.push(descriptorCesar(textForDecrypt.split(""), i + 1));
   }
-  console.log(result);
+  const decrypted = result.reduce((accumulator, value) => accumulator + value);
   document.getElementById(
     "imprimir_descript"
-  ).innerHTML = `Descriptografado: ${result.reduce(
-    (total, value) => total + value
-  )}`;
+  ).innerHTML = `Descriptografado: ${decrypted}`;
 }
 function reset() {
   document.getElementById("text").value = "";
   document.getElementById("spacing").value = "";
   document.getElementById("result").textContent = "";
   document.getElementById("imprimir_descript").textContent = "";
+  document.getElementById("decript").value = "";
 }
